e26cb8e42a1facd532e4357777e602a2c33271d8
Daria Zharkova <dariazharkova1991@gmail.com>
2025-04-30 22:24:12 +0300

    update PATH_DB

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { readContacts } from './readContacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = await readContacts();
+    const updatedData = [...data, updatedContacts];
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedData), 'utf8');
+  } catch (err) {
+    console.error('Помилка запису у файл:', err);
+  }
+};
